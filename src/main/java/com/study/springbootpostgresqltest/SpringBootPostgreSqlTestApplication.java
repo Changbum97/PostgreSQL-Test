fc52e6c3dae20965c6622dd85fa955b219525baf
Changbum97 <chb2005@naver.com>
2023-03-21 23:35:42 +0900

    first commit

---
+++ src/main/java/com/study/springbootpostgresqltest/SpringBootPostgreSqlTestApplication.java
@@ -0,0 +1,13 @@
+package com.study.springbootpostgresqltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPostgreSqlTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPostgreSqlTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/springbootpostgresqltest/SpringBootPostgreSqlTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springbootpostgresqltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPostgreSqlTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
